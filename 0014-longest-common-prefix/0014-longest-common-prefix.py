0f866cf7f4d025fdfc5d0f2bd6d26e769d151ac6
ujwal <ujwaldaivikar@gmail.com>
2026-07-01 10:37:16 +0530

    Time: 0 ms (100%), Space: 12.4 MB (68.34%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        
+        first_str = strs[0]
+        
+        for i in range(len(first_str)):
+            char = first_str[i]
+            
+            for string in strs[1:]:
+                if i >= len(string) or string[i] != char:
+                    return first_str[:i]
+                    
+        return first_str
