1bc7e47cb4da220bda95286d2fa7ec9c1f5c2f57
ujwal <ujwaldaivikar@gmail.com>
2026-07-04 20:25:24 +0530

    Time: 347 ms (82.95%), Space: 12.4 MB (86.17%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -10,12 +10,12 @@ class Solution(object):
             len2 = self.expandAroundCenter(s, i, i + 1)
             max_len = max(len1, len2)
             
-            if max_len > (end - start):
+            if max_len > end - start:
                 start = i - (max_len - 1) // 2
                 end = i + max_len // 2
                 
         return s[start:end + 1]
-        
+    
     def expandAroundCenter(self, s, left, right):
         while left >= 0 and right < len(s) and s[left] == s[right]:
             left -= 1
