801e2e60e4bd1660bbce0eff88838d16048a0887
ujwal <ujwaldaivikar@gmail.com>
2026-07-06 08:45:16 +0530

    Time: 3 ms (28.14%), Space: 12.3 MB (68.15%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,10 +4,8 @@ class Solution(object):
             return ""
         
         first_str = strs[0]
-        
         for i in range(len(first_str)):
             char = first_str[i]
-            
             for string in strs[1:]:
                 if i >= len(string) or string[i] != char:
                     return first_str[:i]
