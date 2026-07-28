5ef87eaeec29383f6d156dc58ffbbf6d78146fc4
ujwal <ujwaldaivikar@gmail.com>
2026-07-28 20:37:47 +0530

    Time: 7 ms (61.38%), Space: 12.3 MB (95.82%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        word_set = set(wordDict)
+        n = len(s)
+        
+        # dp[i] means s[:i] can be segmented using dictionary words
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break  # Found a valid segmentation for length i
+
+        return dp[n]
