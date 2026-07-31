fdf09e74e2e4a60b0ef19af27d45808025b09b27
ujwal <ujwaldaivikar@gmail.com>
2026-07-31 10:22:36 +0530

    Time: 6 ms (61.41%), Space: 13.5 MB (49.31%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        last_non_zero = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                # Swap non-zero element into the next available slot
+                nums[last_non_zero], nums[i] = nums[i], nums[last_non_zero]
+                last_non_zero += 1
