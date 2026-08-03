c1c7385b9224c32dfda2f1f5b93a05ed61faef75
ujwal <ujwaldaivikar@gmail.com>
2026-08-03 13:10:27 +0530

    Time: 5 ms (25.61%), Space: 19.8 MB (76.79%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            # Swap characters at left and right pointers
+            s[left], s[right] = s[right], s[left]
+            
+            # Move pointers toward the center
+            left += 1
+            right -= 1
