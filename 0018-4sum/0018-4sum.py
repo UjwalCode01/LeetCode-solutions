cfaaaba5b1ece95ed77be270bae890b031090fad
ujwal <ujwaldaivikar@gmail.com>
2026-08-25 21:16:05 +0530

    Time: 831 ms (6.62%), Space: 12.3 MB (63.73%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -3,29 +3,31 @@ class Solution(object):
         nums.sort()
         res = []
         n = len(nums)
-        
+
         for i in range(n - 3):
+            # Skip duplicate elements for first position
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-                
+            
             for j in range(i + 1, n - 2):
+                # Skip duplicate elements for second position
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
-                    
-                left = j + 1
-                right = n - 1
                 
+                left, right = j + 1, n - 1
                 while left < right:
                     total = nums[i] + nums[j] + nums[left] + nums[right]
                     
                     if total == target:
                         res.append([nums[i], nums[j], nums[left], nums[right]])
-                        while left < right and nums[left] == nums[left + 1]:
-                            left += 1
-                        while left < right and nums[right] == nums[right - 1]:
-                            right -= 1
                         left += 1
                         right -= 1
+                        
+                        # Skip duplicate elements for third and fourth positions
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
                     elif total < target:
                         left += 1
                     else:
