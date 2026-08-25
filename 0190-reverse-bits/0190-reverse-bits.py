ece1c06f523ca71d7a7593bda120f77752f221b2
ujwal <ujwaldaivikar@gmail.com>
2026-08-25 21:03:11 +0530

    Time: 16 ms (64.54%), Space: 12.4 MB (53.57%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def reverseBits(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
         res = 0
         for _ in range(32):
             res = (res << 1) | (n & 1)
