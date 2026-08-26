d5a7b3cefd918b0729281cd73e621e64671a765e
ujwal <ujwaldaivikar@gmail.com>
2026-08-26 08:55:06 +0530

    Time: 0 ms (100%), Space: 13.3 MB (94.4%) - LeetHub

---
+++ 0598-range-addition-ii/0598-range-addition-ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxCount(self, m, n, ops):
+        """
+        :type m: int
+        :type n: int
+        :type ops: List[List[int]]
+        :rtype: int
+        """
+        if not ops:
+            return m * n
+        
+        # Find the minimum rows and minimum columns affected across all operations
+        min_row = min(op[0] for op in ops)
+        min_col = min(op[1] for op in ops)
+        
+        return min_row * min_col
