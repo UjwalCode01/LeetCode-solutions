74d04ebdab1b30a2fb92ab39995f890d98e590b6
ujwal <ujwaldaivikar@gmail.com>
2026-09-03 18:39:34 +0530

    Time: 152 ms (29%), Space: 12.5 MB (91%) - LeetHub

---
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
@@ -0,0 +1,23 @@
+class MagicDictionary:
+
+    def __init__(self):
+        self.words = []
+
+    def buildDict(self, dictionary):
+        self.words = dictionary
+
+    def search(self, searchWord):
+        for word in self.words:
+            if len(word) != len(searchWord):
+                continue
+
+            diff = 0
+
+            for i in range(len(word)):
+                if word[i] != searchWord[i]:
+                    diff += 1
+
+            if diff == 1:
+                return True
+
+        return False
