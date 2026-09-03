683a9f3b4e015d52b44424bdf8dca9ff386e9eee
ujwal <ujwaldaivikar@gmail.com>
2026-09-03 19:54:34 +0530

    Time: 5 ms (14.58%), Space: 12.5 MB (22.19%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,25 +1,10 @@
 class Solution(object):
     def simplifyPath(self, path):
-        """
-        :type path: str
-        :rtype: str
-        """
         stack = []
-        
-        # Path ko '/' ke basis par split karenge
-        components = path.split('/')
-        
-        for portion in components:
-            # Agar empty string hai ya '.' hai, toh ignore karenge
-            if portion == "" or portion == ".":
-                continue
-            # Agar '..' hai, toh ek step piche jaane ke liye pop karenge
-            elif portion == "..":
+        for token in path.split('/'):
+            if token == '..':
                 if stack:
                     stack.pop()
-            # Baki kisi bhi valid naam ko stack mein add karenge
-            else:
-                stack.append(portion)
-                
-        # Last mein stack ke elements ko '/' se join karke return karenge
-        return "/" + "/".join(stack)
+            elif token and token != '.':
+                stack.append(token)
+        return '/' + '/'.join(stack)
